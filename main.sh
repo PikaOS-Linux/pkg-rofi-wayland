bee42368c581a68f6b27f4eeea29b900bd14ae9d
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:33:23 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,6 @@
 
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-apt install pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew"
 # Clone Upstream
 git clone --recurse-submodules https://github.com/lbonn/rofi.git
 cd rofi
@@ -15,8 +9,6 @@ git checkout 1.7.5+wayland1
 cp -rvf ../debian ./
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
